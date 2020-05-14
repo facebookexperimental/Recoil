8047748968dc1b30c242ebf1278084ad4b20cc0a
Christian Santos <chrissantos1995@gmail.com>
2020-05-14 03:36:06 -0400

    site description and title

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -68,8 +68,7 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="A state management library for React.">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
