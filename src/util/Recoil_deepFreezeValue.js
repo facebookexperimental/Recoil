40da60114b8ae40139249a42b79444e8c7877554
Jirat Ki <saakyz@gmail.com>
2020-05-25 17:40:41 -0700

    Fix call stack size exceeded when set atom value with object that has circular reference (#153)
    Summary:
    ## Background
    
    fix https://github.com/facebookexperimental/Recoil/issues/54
    
    Set atom value with an object that has circular reference inside will cause an error
    
    ```
    RangeError: Maximum call stack size exceeded
    ```
    
    ## What's changed
    
    - In `deepFreezeValue`  Check if a property is already frozen
    Pull Request resolved: https://github.com/facebookexperimental/Recoil/pull/153
    
    Reviewed By: mondaychen
    
    Differential Revision: D21712576
    
    Pulled By: drarmstr
    
    fbshipit-source-id: f44e9ee303e2bbf566cc18f09f64f2b0ade14123

--- src/util/Recoil_deepFreezeValue.js
+++ src/util/Recoil_deepFreezeValue.js
@@ -63,9 +63,13 @@ function deepFreezeValue(value: mixed) {
   }
 
   Object.freeze(value); // Make all properties read-only
-  for (const prop in value) {
-    if (value.hasOwnProperty(prop)) {
-      deepFreezeValue(value[prop]);
+  for (const key in value) {
+    if (value.hasOwnProperty(key)) {
+      const prop = value[key];
+      // Prevent infinite recurssion for circular references.
+      if (typeof prop === 'object' && prop != null && !Object.isFrozen(prop)) {
+        deepFreezeValue(prop);
+      }
     }
   }
   Object.seal(value); // This also makes existing properties non-configurable.
