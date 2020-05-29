2df74404d35b2d16f7d9e99fe2d3fe9ac35556b5
Jared Palmer <jared@palmer.net>
2020-05-29 13:35:35 -0400

    Sort .babelrc and rollup plugins array

--- rollup.config.js
+++ rollup.config.js
@@ -16,9 +16,10 @@ const config = mode => ({
     babel({
       presets: ['@babel/preset-react', '@babel/preset-flow'],
       plugins: [
+        '@babel/plugin-proposal-class-properties',
         '@babel/plugin-proposal-nullish-coalescing-operator',
         '@babel/plugin-proposal-optional-chaining',
-        '@babel/plugin-proposal-class-properties',
+        '@babel/transform-flow-strip-types',
       ],
       babelHelpers: 'bundled',
     }),
