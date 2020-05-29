1e7ec2b10d2f93b71fb704319a4301110e7d0c27
Jared Palmer <jared@palmer.net>
2020-05-29 12:59:43 -0400

    Remove utils, move exports to _index, fix pkg.json

--- src/Recoil_index.js
+++ src/Recoil_index.js
@@ -24,12 +24,17 @@ export type {
   ResetRecoilState,
 } from './recoil_values/Recoil_selector';
 export type {
+  Parameter,
+  SelectorFamilyOptions,
+} from './recoil_values/Recoil_selectorFamily';
+export type {
   RecoilValue,
   RecoilState,
   RecoilValueReadOnly,
 } from './core/Recoil_RecoilValue';
 
 const atom = require('./recoil_values/Recoil_atom');
+const atomFamily = require('./recoil_values/Recoil_atomFamily');
 const {
   useRecoilCallback,
   useRecoilState,
@@ -46,6 +51,17 @@ const {DefaultValue} = require('./core/Recoil_Node');
 const {RecoilRoot} = require('./components/Recoil_RecoilRoot.react');
 const {isRecoilValue} = require('./core/Recoil_RecoilValue');
 const selector = require('./recoil_values/Recoil_selector');
+const selectorFamily = require('./recoil_values/Recoil_selectorFamily');
+const readOnlySelector = require('./recoil_values/Recoil_readOnlySelector');
+const constSelector = require('./recoil_values/Recoil_const');
+const errorSelector = require('./recoil_values/Recoil_error');
+const Link = require('./components/Recoil_Link.react');
+const {
+  noWait,
+  waitForAll,
+  waitForAny,
+  waitForNone,
+} = require('./recoil_values/Recoil_WaitFor');
 
 module.exports = {
   // Types
@@ -76,4 +92,20 @@ module.exports = {
 
   // Other functions
   isRecoilValue,
+
+  // Convenience RecoilValues
+  atomFamily,
+  selectorFamily,
+  constSelector,
+  errorSelector,
+  readOnlySelector,
+
+  // Concurrency Helpers
+  noWait,
+  waitForNone,
+  waitForAny,
+  waitForAll,
+
+  // Components
+  Link_UNSTABLE: Link,
 };
