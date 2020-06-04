1fc4497e49d68d2991ebda6d5414a6f079d9f5f8
Jon Thompson <jonthomp@users.noreply.github.com>
2020-06-04 13:08:06 -0700

    Fix cannot destructure property error of undefined (#199)
    Summary:
    Adds a default value `{}` to the 3rd argument of `recoverableViolation` so that it has something to try and destructure `error` from when nothing is passed at call-sites.
    
    Note: the third argument isn't currently used anywhere in this codebase
    Pull Request resolved: https://github.com/facebookexperimental/Recoil/pull/199
    
    Reviewed By: drarmstr
    
    Differential Revision: D21813603
    
    Pulled By: mondaychen
    
    fbshipit-source-id: 1b8eb4754b0083ddca64a8fdb3818b17e105f4da

--- src/util/Recoil_recoverableViolation.js
+++ src/util/Recoil_recoverableViolation.js
@@ -14,15 +14,20 @@
 // @fb-only: const recoverableViolation = require('recoverableViolation');
 
 // prettier-ignore
+/* eslint-disable no-lone-blocks */
+// this {} block is necessary to keep prettier off on both internal repo and github
+{
 function recoverableViolation( // @oss-only
   message: string, // @oss-only
   projectName: 'recoil', // @oss-only
-  {error}: {|error?: Error|}, // @oss-only
+  {error}: {|error?: Error|} = {}, // @oss-only
 ): null { // @oss-only
   if (__DEV__) { // @oss-only
     console.error(message, error); // @oss-only
   } // @oss-only
   return null; // @oss-only
 } // @oss-only
+}
+/* eslint-enable no-lone-blocks */
 
 module.exports = recoverableViolation;
