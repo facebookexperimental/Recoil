ca4f536c549815bea373d041e1600f42b7f92dd5
Wilbert Abreu <wilbert.a.abreu@aexp.com>
2020-06-05 08:42:32 -0400

    feat(rollupPluginCopy): Using rollup-plugin-copy instead of postbuild script

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import {terser} from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy';
 
 const config = mode => ({
   input: 'src/Recoil_index.js',
@@ -39,6 +40,9 @@ const config = mode => ({
     commonjs(),
     replace({
       'process.env.NODE_ENV': JSON.stringify(mode),
+    }),
+    copy({
+      targets: [{src: 'src/npm/index.js', dest: 'dist'}],
     }),
     mode === 'development' ? undefined : terser({mangle: false}),
   ],
