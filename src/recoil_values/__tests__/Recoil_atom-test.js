a0991727a60eba8becc225d09132082cfe548476
Douglas Armstrong <dougarmstrong@fb.com>
2020-06-22 17:17:20 -0700

    Recoil Atom Effects
    Summary:
    Introduce the concept of effects for Recoil Atoms based on [this proposal](https://fb.quip.com/bauvAeBEX5Ee).  This is a similar concept of using React `useEffect()` for side-effects on components, only for atoms.  It can be challenging to use `useEffect()` with Recoil atoms because the Recoil hierarchy is orthogonal to the React component hierarchy, and we have to worry about lifetime and singleton patterns.  Recoil Atom Effects are an array of callbacks which will be called the first time the atom is used with a `<RecoilRoot>`, so they are always part of a React context.  Atom Effects can be used for:
    
    * Synchronization with browser URI history
    * Persistence to local storage
    * Bi-directional synchronization with remote asynchronous state
    * Maintaining state history
    * other side-effects such as "set on get"
    * &c.
    
    # Proposed Atom Effects
    
    ```
    type AtomOptions<T> = {
      key: string,
      default: T | RecoilValue<T> | Promise<T>,
      ...
    
      effects: $ReadOnlyArray<AtomEffect<T>>,
    };
    
    // Effect is called the first time node is used with a <RecoilRoot>
    type AtomEffect<T> = ({
      node: RecoilState<T>,
      trigger: 'get' | 'set',
    
      // Can call either synchronously to initialize value or async to change it later
      setSelf: (T | DefaultValue | (T => T | DefaultValue)) => void,
      resetSelf: () => void,
      getSnapshot: () => Snapshot,
    
      // Subscribe to events
      // Called when React batch ends, but before global RecoilTransactionObserver
      onSet: ((newValue: T | DefaultValue, oldValue: T | DefaultValue) => void) => void,
    }) => void | () => void; // Return optional cleanup handler
    ```
    
    ### Current Diff
    This diff introduces a subset of the atom effect functionality:
    
    ```
    // Effect is called the first time node is used with a <RecoilRoot>
    type AtomEffect<T> = ({
      node: RecoilState<T>,
      trigger: 'get' | 'set',
    
      setSelf: (T | DefaultValue | (T => T | DefaultValue)) => void,
      resetSelf: () => void,
      getSnapshot: () => Snapshot,
    }) => void;
    ```
    
    ### Example Usage
    Example use of an atom as a local cache of state that can be bi-directionally synchronized with remote async storage.
    ```
    const myAtom = atom({
      key: 'MyAtom',
      default: undefined,
    
      effects: [({node, setSelf, onSet}) => {
        const storage = getStorage(node.key);
    
        // set a value for synchronous init that overrides default
        setSelf(storage.get(value));
    
        // Subscribe to storage updates
        storage.subscribe(value => setSelf(value));
    
        // Subscribe local changes to update storage
        onSet(value =>
          value === undefined
            ? storage.reset()
            : storage.set(value)
        );
    
        // Subscribe to cleanup
        onCleanup(() => storage.unsubscribe());
      }],
    });
    ```
    
    Differential Revision: D21919545
    
    fbshipit-source-id: 9b43bd18fef5fdc0b17a2c6bd9b4aac500577ddc

--- src/recoil_values/__tests__/Recoil_atom-test.js
+++ src/recoil_values/__tests__/Recoil_atom-test.js
@@ -9,13 +9,20 @@
 
 import type {Store} from 'Recoil_State';
 
+const React = require('React');
 const {act} = require('ReactTestUtils');
 
+const {DEFAULT_VALUE} = require('../../core/Recoil_Node');
 const {
   getRecoilValueAsLoadable,
   setRecoilValue,
 } = require('../../core/Recoil_RecoilValueInterface');
-const {makeStore} = require('../../testing/Recoil_TestingUtils');
+const {
+  ReadsAtom,
+  componentThatReadsAndWritesAtom,
+  makeStore,
+  renderElements,
+} = require('../../testing/Recoil_TestingUtils');
 const atom = require('../Recoil_atom');
 
 let store: Store;
@@ -31,6 +38,10 @@ function set(recoilValue, value) {
   setRecoilValue(store, recoilValue, value);
 }
 
+function reset(recoilValue) {
+  setRecoilValue(store, recoilValue, DEFAULT_VALUE);
+}
+
 test('atom can read and write value', () => {
   const myAtom = atom<string>({
     key: 'atom with default',
@@ -41,36 +52,236 @@ test('atom can read and write value', () => {
   expect(get(myAtom)).toBe('VALUE');
 });
 
-test('atom can store null and undefined', () => {
-  const myAtom = atom<?string>({
-    key: 'atom with default for null and undefined',
-    default: 'DEFAULT',
+describe('Valid values', () => {
+  test('atom can store null and undefined', () => {
+    const myAtom = atom<?string>({
+      key: 'atom with default for null and undefined',
+      default: 'DEFAULT',
+    });
+    expect(get(myAtom)).toBe('DEFAULT');
+    act(() => set(myAtom, 'VALUE'));
+    expect(get(myAtom)).toBe('VALUE');
+    act(() => set(myAtom, null));
+    expect(get(myAtom)).toBe(null);
+    act(() => set(myAtom, undefined));
+    expect(get(myAtom)).toBe(undefined);
+    act(() => set(myAtom, 'VALUE'));
+    expect(get(myAtom)).toBe('VALUE');
   });
-  expect(get(myAtom)).toBe('DEFAULT');
-  act(() => set(myAtom, 'VALUE'));
-  expect(get(myAtom)).toBe('VALUE');
-  act(() => set(myAtom, null));
-  expect(get(myAtom)).toBe(null);
-  act(() => set(myAtom, undefined));
-  expect(get(myAtom)).toBe(undefined);
-  act(() => set(myAtom, 'VALUE'));
-  expect(get(myAtom)).toBe('VALUE');
+
+  test('atom can store a circular reference object', () => {
+    class Circular {
+      self: Circular;
+
+      constructor() {
+        this.self = this;
+      }
+    }
+    const circular = new Circular();
+    const myAtom = atom<?Circular>({
+      key: 'atom',
+      default: undefined,
+    });
+    expect(get(myAtom)).toBe(undefined);
+    act(() => set(myAtom, circular));
+    expect(get(myAtom)).toBe(circular);
+  });
 });
 
-test('atom can store a circular reference object', () => {
-  class Circular {
-    self: Circular;
-
-    constructor() {
-      this.self = this;
-    }
-  }
-  const circular = new Circular();
-  const myAtom = atom<?Circular>({
-    key: 'atom',
-    default: undefined,
+describe('Effects', () => {
+  test('effect', () => {
+    let inited = false;
+    const myAtom = atom({
+      key: 'atom effect',
+      default: 'DEFAULT',
+      effects_UNSTABLE: [
+        ({node, trigger, setSelf}) => {
+          inited = true;
+          expect(trigger).toEqual('get');
+          expect(node).toBe(myAtom);
+          setSelf('EFFECT');
+        },
+      ],
+    });
+    expect(get(myAtom)).toEqual('EFFECT');
+    expect(inited).toEqual(true);
   });
-  expect(get(myAtom)).toBe(undefined);
-  act(() => set(myAtom, circular));
-  expect(get(myAtom)).toBe(circular);
+
+  test('order of effects', () => {
+    const myAtom = atom({
+      key: 'atom effect order',
+      default: 'DEFAULT',
+      effects_UNSTABLE: [
+        ({setSelf}) => {
+          setSelf(x => {
+            expect(x).toEqual('DEFAULT');
+            return 'EFFECT 1a';
+          });
+          setSelf(x => {
+            expect(x).toEqual('EFFECT 1a');
+            return 'EFFECT 1b';
+          });
+        },
+        ({setSelf}) => {
+          setSelf(x => {
+            expect(x).toEqual('EFFECT 1b');
+            return 'EFFECT 2';
+          });
+        },
+        () => {},
+      ],
+    });
+    expect(get(myAtom)).toEqual('EFFECT 2');
+  });
+
+  test('reset during init', () => {
+    const myAtom = atom({
+      key: 'atom effect reset',
+      default: 'DEFAULT',
+      effects_UNSTABLE: [
+        ({setSelf}) => setSelf('INIT'),
+        ({resetSelf}) => resetSelf(),
+      ],
+    });
+    expect(get(myAtom)).toEqual('DEFAULT');
+  });
+
+  test('init to undefined', () => {
+    const myAtom = atom({
+      key: 'atom effect init undefined',
+      default: 'DEFAULT',
+      effects_UNSTABLE: [
+        ({setSelf}) => setSelf('INIT'),
+        ({setSelf}) => setSelf(),
+      ],
+    });
+    expect(get(myAtom)).toEqual(undefined);
+  });
+
+  test('init on set', () => {
+    let inited = 0;
+    const myAtom = atom({
+      key: 'atom effect - init on set',
+      default: 'DEFAULT',
+      effects_UNSTABLE: [
+        ({setSelf, trigger}) => {
+          inited++;
+          setSelf('INIT');
+          expect(trigger).toEqual('set');
+        },
+      ],
+    });
+    set(myAtom, 'SET');
+    expect(get(myAtom)).toEqual('SET');
+    expect(inited).toEqual(1);
+    reset(myAtom);
+    expect(get(myAtom)).toEqual('DEFAULT');
+    expect(inited).toEqual(1);
+  });
+
+  test('init from other atom', () => {
+    const myAtom = atom({
+      key: 'atom effect - init from other atom',
+      default: 'DEFAULT',
+      effects_UNSTABLE: [
+        ({setSelf, getSnapshot}) => {
+          const snapshot = getSnapshot();
+          const otherValue = snapshot.getLoadable(otherAtom).contents;
+          expect(otherValue).toEqual('OTHER');
+          setSelf(otherValue);
+        },
+      ],
+    });
+
+    const otherAtom = atom({
+      key: 'atom effect - other atom',
+      default: 'OTHER',
+    });
+
+    expect(get(myAtom)).toEqual('OTHER');
+  });
+
+  test('async set', async () => {
+    let setAtom, resetAtom;
+
+    const myAtom = atom({
+      key: 'atom effect init set',
+      default: 'DEFAULT',
+      effects_UNSTABLE: [
+        ({setSelf, resetSelf}) => {
+          setAtom = setSelf;
+          resetAtom = resetSelf;
+          setSelf(x => {
+            expect(x).toEqual('DEFAULT');
+            return 'INIT';
+          });
+        },
+      ],
+    });
+
+    const c = renderElements(<ReadsAtom atom={myAtom} />);
+
+    expect(c.textContent).toEqual('"INIT"');
+
+    // Test async set
+    act(() =>
+      setAtom(value => {
+        expect(value).toEqual('INIT');
+        return 'SET';
+      }),
+    );
+    expect(c.textContent).toEqual('"SET"');
+
+    // Test async change
+    act(() =>
+      setAtom(value => {
+        expect(value).toEqual('SET');
+        return 'CHANGE';
+      }),
+    );
+    expect(c.textContent).toEqual('"CHANGE"');
+
+    // Test reset
+    act(resetAtom);
+    expect(c.textContent).toEqual('"DEFAULT"');
+
+    // Test setting to undefined
+    act(() =>
+      setAtom(value => {
+        expect(value).toEqual('DEFAULT');
+        return undefined;
+      }),
+    );
+    expect(c.textContent).toEqual('');
+  });
+
+  test('once per root', async () => {
+    let inited = 0;
+    const myAtom = atom({
+      key: 'atom effect once per root',
+      default: 'DEFAULT',
+      effects_UNSTABLE: [
+        ({setSelf}) => {
+          inited++;
+          setSelf('INIT');
+        },
+      ],
+    });
+
+    const [ReadsWritesAtom, setAtom] = componentThatReadsAndWritesAtom(myAtom);
+
+    // effect is called once per <RecoilRoot>
+    const c1 = renderElements(<ReadsWritesAtom />);
+    const c2 = renderElements(<ReadsAtom atom={myAtom} />);
+
+    expect(c1.textContent).toEqual('"INIT"');
+    expect(c2.textContent).toEqual('"INIT"');
+
+    act(() => setAtom('SET'));
+
+    expect(c1.textContent).toEqual('"SET"');
+    expect(c2.textContent).toEqual('"INIT"');
+
+    expect(inited).toEqual(2);
+  });
 });
