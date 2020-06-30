3cb1ad8e6ab2ab2ccea52ed133e99bab3e733637
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-06-30 21:39:13 +0200

    Fix formatting

--- src/core/Recoil_Snapshot.js
+++ src/core/Recoil_Snapshot.js
@@ -100,7 +100,7 @@ class Snapshot {
     const deps = this._store
       .getState()
       .currentTree.nodeDeps.get(recoilValue.key);
-    return (function* () {
+    return (function*() {
       for (const key of deps ?? []) {
         yield nullthrows(recoilValues.get(key));
       }
