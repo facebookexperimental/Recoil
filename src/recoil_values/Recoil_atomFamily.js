ea7f983f8615cd6bf8e27d463121183a6d1d5c5c
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-06-30 21:43:38 +0200

    Fix formatting

--- src/recoil_values/Recoil_atomFamily.js
+++ src/recoil_values/Recoil_atomFamily.js
@@ -89,13 +89,13 @@ function atomFamily<T, P: Parameter>(
   // @fb-only: if (
   // @fb-only: options.scopeRules_APPEND_ONLY_READ_THE_DOCS
   // @fb-only: ) {
-  // @fb-only: legacyAtom = parameterizedScopedAtomLegacy<T | DefaultValue, P>({
-  // @fb-only: ...legacyAtomOptions,
-  // @fb-only: scopeRules_APPEND_ONLY_READ_THE_DOCS:
-  // @fb-only: options.scopeRules_APPEND_ONLY_READ_THE_DOCS,
-  // @fb-only: });
+    // @fb-only: legacyAtom = parameterizedScopedAtomLegacy<T | DefaultValue, P>({
+      // @fb-only: ...legacyAtomOptions,
+      // @fb-only: scopeRules_APPEND_ONLY_READ_THE_DOCS:
+        // @fb-only: options.scopeRules_APPEND_ONLY_READ_THE_DOCS,
+    // @fb-only: });
   // @fb-only: } else {
-  legacyAtom = atom<T | DefaultValue>(legacyAtomOptions);
+    legacyAtom = atom<T | DefaultValue>(legacyAtomOptions);
   // @fb-only: }
 
   // Selector to calculate the default value based on any persisted legacy atoms
@@ -134,11 +134,11 @@ function atomFamily<T, P: Parameter>(
     const newAtom = atom<T>({
       ...options,
       key: `${options.key}__${stableStringify(params) ?? 'void'}`,
-      default: atomFamilyDefault(params)
+      default: atomFamilyDefault(params),
       // prettier-ignore
       // @fb-only: scopeRules_APPEND_ONLY_READ_THE_DOCS: mapScopeRules(
-      // @fb-only: options.scopeRules_APPEND_ONLY_READ_THE_DOCS,
-      // @fb-only: params,
+        // @fb-only: options.scopeRules_APPEND_ONLY_READ_THE_DOCS,
+        // @fb-only: params,
       // @fb-only: )
     });
 
