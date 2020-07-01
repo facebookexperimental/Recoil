d90e8b72b6ba9a790427e66eae480d28e04aa841
Douglas Armstrong <dougarmstrong@fb.com>
2020-06-30 17:27:39 -0700

    Restore sprintf() in expectationViolation() (#421)
    Summary:
    Pull Request resolved: https://github.com/facebookexperimental/Recoil/pull/421
    
    The `Recoil_sprintf.js` file was removed by the DeadJSCode reaper in D22184459 (https://github.com/facebookexperimental/recoil/commit/d4343b68eb06c6b58720adb5d0e2d3e4001921cb) since it was only referenced by `oss-only` code.  Solve that by just placing that function in `Recoil_expectationViolation.js`
    
    Differential Revision: D22323789
    
    fbshipit-source-id: 6c665805121a470e7116fdd53ed3139941e56279

--- src/util/Recoil_expectationViolation.js
+++ src/util/Recoil_expectationViolation.js
@@ -8,20 +8,36 @@
  * @flow strict-local
  * @format
  */
-
 'use strict';
 
 // @fb-only: const expectationViolation = require('expectationViolation');
 
-const sprintf = require('./Recoil_sprintf'); // @oss-only
+// The {} blocks are necessary to keep prettier off on internal repo
+/* eslint-disable no-lone-blocks */
+
 // prettier-ignore
+function sprintf(format: string, ...args: Array<mixed>): string { // @oss-only
+// @fb-only: {
+  let index = 0; // @oss-only
+  return format.replace(/%s/g, () => String(args[index++])); // @oss-only
+// @fb-only: }
+} // @oss-only
+
+// prettier-ignore
+const sprintf = require('./Recoil_sprintf'); // @oss-only
 function expectationViolation(format: string, ...args: $ReadOnlyArray<mixed>) { // @oss-only
+// @fb-only: {
   if (__DEV__) { // @oss-only
+  // @fb-only: {
     const message = sprintf.call(null, format, ...args); // @oss-only
     const error = new Error(message); // @oss-only
     error.name = 'Expectation Violation'; // @oss-only
     console.error(error); // @oss-only
+  // @fb-only: }
   } // @oss-only
+// @fb-only: }
 } // @oss-only
+
+/* eslint-enable no-lone-blocks */
 
 module.exports = expectationViolation;
