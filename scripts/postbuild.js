2a58f6b546c97c9149e84fdf4ed33940ed7475f6
mondaychen <monday.chen@gmail.com>
2020-07-03 18:14:43 -0700

    fix bad file path (#431)
    Summary:
    I got confused on the cwd of the scripts. My bad.
    
    Pull Request resolved: https://github.com/facebookexperimental/Recoil/pull/431
    
    Reviewed By: drarmstr
    
    Differential Revision: D22380449
    
    Pulled By: mondaychen
    
    fbshipit-source-id: 7916f6e9588325f45a78be1f6733af1ec2cd0e3d

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -7,8 +7,8 @@ function logErrors(err) {
 }
 
 // For CommonJS
-fs.copyFile('./npm/index.js', './dist/index.js', logErrors);
+fs.copyFile('./scripts/npm/index.js', './dist/index.js', logErrors);
 fs.copyFile('./typescript/index.d.ts', './dist/index.d.ts', logErrors);
 // For ESM
-fs.copyFile('./npm/index.js', './esm/index.js', logErrors);
+fs.copyFile('./scripts/npm/index.js', './esm/index.js', logErrors);
 fs.copyFile('./typescript/index.d.ts', './esm/index.d.ts', logErrors);
