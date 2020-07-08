86ad90335d0850031e50ec3c8808881ee8c74a2d
Brian Chen <brian.chen@tabcorp.com.au>
2020-07-09 09:56:09 +1000

    compile down to es5

--- rollup.config.js
+++ rollup.config.js
@@ -23,7 +23,16 @@ const config = modes.map(mode => ({
   external: ['react', 'react-dom'],
   plugins: [
     babel({
-      presets: ['@babel/preset-react', '@babel/preset-flow'],
+      presets: [
+        '@babel/preset-react',
+        '@babel/preset-flow',
+        [
+          '@babel/preset-env',
+          {
+            targets: {browsers: ['last 2 versions', 'safari >= 7']},
+          },
+        ],
+      ],
       plugins: [
         'babel-preset-fbjs/plugins/dev-expression',
         '@babel/plugin-proposal-class-properties',
