5ca385ade41e67d237b0d02a326ea8a90b3ff335
Pocket7878 <poketo7878@gmail.com>
2020-07-09 11:25:46 -0700

    fix: Fix rollup.config.js to handling CommonJS, ESM, ESM for Browser, UMD correctly. (#433)
    Summary:
    FIX issue from previous PR: https://github.com/facebookexperimental/Recoil/pull/413
    
    Pull Request resolved: https://github.com/facebookexperimental/Recoil/pull/433
    
    Reviewed By: drarmstr
    
    Differential Revision: D22419761
    
    Pulled By: mondaychen
    
    fbshipit-source-id: c9f86304dfc33abd755db249ea9b3d2c30d16ff8

--- rollup.config.js
+++ rollup.config.js
@@ -4,53 +4,113 @@ import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import {terser} from 'rollup-plugin-terser';
 
-const modes = ['development', 'production'];
+const inputFile = 'src/Recoil_index.js';
+const externalLibs = ['react', 'react-dom'];
 
-const config = modes.map(mode => ({
-  input: 'src/Recoil_index.js',
-  output: [
-    {
-      file: `esm/recoil.${mode}.js`,
-      format: 'es',
-      exports: 'named',
+const commonPlugins = [
+  babel({
+    presets: ['@babel/preset-react', '@babel/preset-flow'],
+    plugins: [
+      'babel-preset-fbjs/plugins/dev-expression',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/transform-flow-strip-types',
+    ],
+    babelHelpers: 'bundled',
+  }),
+  {
+    resolveId: source => {
+      if (source === 'React') {
+        return {id: 'react', external: true};
+      }
+      if (source === 'ReactDOM') {
+        return {id: 'react-dom', external: true};
+      }
+      return null;
     },
-    {
-      file: `dist/recoil.${mode}.js`,
+  },
+  nodeResolve(),
+  commonjs(),
+];
+
+const developmentPlugins = [
+  ...commonPlugins,
+  replace({
+    'process.env.NODE_ENV': JSON.stringify('development'),
+  }),
+];
+
+const productionPlugins = [
+  ...commonPlugins,
+  replace({
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  }),
+  terser({mangle: false}),
+];
+
+const configs = [
+  // CommonJS
+  {
+    input: inputFile,
+    output: {
+      file: `lib/recoil.js`,
       format: 'cjs',
       exports: 'named',
     },
-  ],
-  external: ['react', 'react-dom'],
-  plugins: [
-    babel({
-      presets: ['@babel/preset-react', '@babel/preset-flow'],
-      plugins: [
-        'babel-preset-fbjs/plugins/dev-expression',
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-        '@babel/plugin-proposal-optional-chaining',
-        '@babel/transform-flow-strip-types',
-      ],
-      babelHelpers: 'bundled',
-    }),
-    {
-      resolveId: source => {
-        if (source === 'React') {
-          return {id: 'react', external: true};
-        }
-        if (source === 'ReactDOM') {
-          return {id: 'react-dom', external: true};
-        }
-        return null;
-      },
-    },
-    nodeResolve(),
-    commonjs(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(mode),
-    }),
-    mode === 'development' ? undefined : terser({mangle: false}),
-  ],
-}));
+    external: externalLibs,
+    plugins: commonPlugins,
+  },
 
-export default config;
+  // ES
+  {
+    input: inputFile,
+    output: {
+      file: `es/recoil.js`,
+      format: 'es',
+      exports: 'named',
+    },
+    external: externalLibs,
+    plugins: commonPlugins,
+  },
+
+  // ES for Browsers
+  {
+    input: inputFile,
+    output: {
+      file: `es/recoil.mjs`,
+      format: 'es',
+      exports: 'named',
+    },
+    external: externalLibs,
+    plugins: productionPlugins,
+  },
+
+  // UMD Development
+  {
+    input: inputFile,
+    output: {
+      file: `dist/recoil.js`,
+      format: 'umd',
+      name: 'Recoil',
+      exports: 'named',
+    },
+    external: externalLibs,
+    plugins: developmentPlugins,
+  },
+
+  // UMD Production
+  {
+    input: inputFile,
+    output: {
+      file: `dist/recoil.min.js`,
+      format: 'umd',
+      name: 'Recoil',
+      exports: 'named',
+    },
+    external: externalLibs,
+    plugins: productionPlugins,
+  },
+];
+
+export default configs;
