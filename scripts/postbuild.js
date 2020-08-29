7f7ab836163fa70e9fb8cd17d0f9d26fd0b3b516
mondaychen <monday.chen@gmail.com>
2020-08-28 23:04:26 -0400

    Merge flow files generation script to postbuild.js

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const {exec} = require('child_process');
 
 function logErrors(err) {
   if (err) {
@@ -6,5 +7,11 @@ function logErrors(err) {
   }
 }
 
-// Copying index.d.ts
+console.log('Copying index.d.ts for Typescript support...');
 fs.copyFile('./typescript/index.d.ts', './index.d.ts', logErrors);
+
+console.log('Generating Flow type files...');
+exec('npx gen-flow-files src --out-dir cjs', err => {
+  logErrors(err);
+  fs.rename('cjs/Recoil_index.js.flow', 'cjs/recoil.js.flow', logErrors);
+});
