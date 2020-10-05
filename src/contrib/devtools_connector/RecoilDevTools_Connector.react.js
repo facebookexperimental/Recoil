4107e957110a5fd9e1d09afd97a2da276fa53c77
Maximiliano Benedetto <maxi@fb.com>
2020-10-05 12:17:06 -0700

    Setting OSS devtools observer
    Summary:
    Using gatekeeper to activate the new observer.
    Connects a recoil store with the DevTools chrome extension.
    Provide initial snapshot and goToSnaphot hook to the extension.
    
    Reviewed By: drarmstr
    
    Differential Revision: D22757783
    
    fbshipit-source-id: 8aa4ab5375417eaa3a9b1ece8951861d2def2ec9

---
+++ src/contrib/devtools_connector/RecoilDevTools_Connector.react.js
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails oncall+recoil
+ * @flow strict-local
+ * @format
+ */
+
+'use strict';
+
+const React = require('React');
+
+const {useEffect, useRef} = require('react');
+const {Snapshot} = require('recoil-oss/core/Recoil_Snapshot');
+const {
+  useGotoRecoilSnapshot,
+  useRecoilSnapshot,
+} = require('recoil-oss/hooks/Recoil_Hooks');
+const useEffectOnce = require('useEffectOnce');
+type Props = $ReadOnly<{
+  name?: string,
+  persitenceLimit?: number,
+  initialSnapshot?: ?Snapshot,
+  devMode?: ?boolean,
+}>;
+
+type ConnectProps = $ReadOnly<{
+  ...Props,
+  goToSnapshot: Snapshot => void,
+}>;
+
+function connect(
+  props: ConnectProps,
+): ?{
+  track: (transactionId: number, snapshot: Snapshot) => void,
+  disconnect: () => void,
+} {
+  if (typeof window === 'undefined') {
+    return null;
+  }
+
+  const devTools =
+    window.__RECOIL_DEVTOOLS_EXTENSION__ &&
+    window.__RECOIL_DEVTOOLS_EXTENSION__.connect &&
+    window.__RECOIL_DEVTOOLS_EXTENSION__.connect(props);
+  return devTools ?? null;
+}
+
+let CONNECTION_INDEX = 0;
+
+/**
+ * @explorer-desc
+ * Recoil Dev Tools Connector
+ */
+function Connector({
+  name = `Recoil Connection ${CONNECTION_INDEX++}`,
+  persitenceLimit = 50,
+  devMode = true,
+}: Props): React.Node {
+  const transactionIdRef = useRef(0);
+  const connectionRef = useRef(null);
+  const goToSnapshot = useGotoRecoilSnapshot();
+  const snapshot = useRecoilSnapshot();
+
+  useEffectOnce(() => {
+    connectionRef.current = connect({
+      name,
+      persitenceLimit,
+      devMode,
+      goToSnapshot,
+      initialSnapshot: snapshot,
+    });
+
+    if (connectionRef.current?.disconnect != null) {
+      return connectionRef.current?.disconnect;
+    }
+  });
+
+  useEffect(() => {
+    const transactionID = transactionIdRef.current++;
+    if (connectionRef.current?.track != null) {
+      connectionRef.current.track(transactionID, snapshot);
+    }
+  }, [snapshot]);
+
+  return null;
+}
+
+module.exports = Connector;
