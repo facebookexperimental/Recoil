92666e8f8e2c8ae1959212836e934d15ce036b02
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-10-30 12:04:32 -0700

    Fix CI on OSS build (#692)
    Summary:
    OSS build CI was failing due to eslint and flowtype.
    
    I've made some fixes to have CI pass for OSS repo.
    
    cc drarmstr
    
    Pull Request resolved: https://github.com/facebookexperimental/Recoil/pull/692
    
    Differential Revision: D24654484
    
    Pulled By: drarmstr
    
    fbshipit-source-id: 45a88f19e3a4f500fa08dbec622c525a43d50884

--- scripts/deploy_nightly_build.js
+++ scripts/deploy_nightly_build.js
@@ -1,13 +1,13 @@
-const package = require('../package.json');
+const {files, version, repository} = require('../package.json');
 const {execSync} = require('child_process');
 
 const DEST_FOLDER = 'nightly-build-files/';
 const DEST_BRANCH = 'nightly';
 
-const COMMIT_MSG = `Publishing a nightly build as ${package.version}`;
+const COMMIT_MSG = `Publishing a nightly build as ${version}`;
 
 const SOURCES = ['CHANGELOG.md', 'LICENSE', 'README.md', 'package.json'].concat(
-  package.files,
+  files,
 );
 
 const cwd = process.cwd();
@@ -17,10 +17,9 @@ console.log(`Remove "${DEST_FOLDER}" folder...`);
 execSync(`rm -rf ${DEST_FOLDER}`, {cwd});
 
 console.log(`Cloning the repository to "${DEST_FOLDER}" folder...`);
-execSync(
-  `git clone -b ${DEST_BRANCH} ${package.repository} ${DEST_FOLDER} --depth 1`,
-  {cwd},
-);
+execSync(`git clone -b ${DEST_BRANCH} ${repository} ${DEST_FOLDER} --depth 1`, {
+  cwd,
+});
 
 console.log('Copying sources...');
 execSync(`cp -r ${SOURCES.join(' ')} ${DEST_FOLDER}`, {cwd});
@@ -32,7 +31,7 @@ execSync(
     'git config --local include.path "$PWD/../.git/config"', // include orginal git config
     'git add .',
     `git commit --allow-empty -m "${COMMIT_MSG}"`,
-    `git push --tags ${package.repository} ${DEST_BRANCH}`,
+    `git push --tags ${repository} ${DEST_BRANCH}`,
   ].join('&&'),
   {cwd},
 );
