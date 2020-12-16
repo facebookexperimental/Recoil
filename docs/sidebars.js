dea9e67050b9f30baca0d46863b2c7769be31f38
Douglas Armstrong <dougarmstrong@fb.com>
2020-12-16 12:48:10 -0800

    [docs] Flatten API Reference (#801)
    * [docs] Clarify atom/selector object freezing
    
    * [docs] Flatten API Reference

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -34,43 +34,27 @@ module.exports = {
       'guides/atom-effects',
       'guides/testing',
       'guides/dev-tools',
-      //   'guides/writing-test',
       //   'guides/code-splitting',
     ],
 
     'API Reference': [
+      'api-reference/core/RecoilRoot',
       {
-        Core: [
-          'api-reference/core/RecoilRoot',
-          {
-            State: [
-              'api-reference/core/atom',
-              'api-reference/core/selector',
-              'api-reference/core/Loadable',
-              'api-reference/core/useRecoilState',
-              'api-reference/core/useRecoilValue',
-              'api-reference/core/useSetRecoilState',
-              'api-reference/core/useResetRecoilState',
-              'api-reference/core/useRecoilValueLoadable',
-              'api-reference/core/useRecoilStateLoadable',
-              'api-reference/core/isRecoilValue',
-              // 'api-reference/core/DefaultValue',
-            ],
-            Snapshots: [
-              'api-reference/core/Snapshot',
-              'api-reference/core/useRecoilTransactionObserver',
-              'api-reference/core/useRecoilSnapshot',
-              'api-reference/core/useGotoRecoilSnapshot',
-            ],
-          },
-          'api-reference/core/useRecoilCallback',
-          {
-            Misc: [
-              'api-reference/core/useRecoilBridgeAcrossReactRoots',
-            ],
-          },
+        'Recoil State': [
+          'api-reference/core/atom',
+          'api-reference/core/selector',
+          'api-reference/core/Loadable',
+          'api-reference/core/useRecoilState',
+          'api-reference/core/useRecoilValue',
+          'api-reference/core/useSetRecoilState',
+          'api-reference/core/useResetRecoilState',
+          'api-reference/core/useRecoilValueLoadable',
+          'api-reference/core/useRecoilStateLoadable',
+          'api-reference/core/isRecoilValue',
+          // 'api-reference/core/DefaultValue',
         ],
       },
+      'api-reference/core/useRecoilCallback',
       {
         Utils: [
           'api-reference/utils/atomFamily',
@@ -81,6 +65,15 @@ module.exports = {
           'api-reference/utils/waitForAny',
           'api-reference/utils/waitForNone',
           'api-reference/utils/noWait',
+        ],
+        Snapshots: [
+          'api-reference/core/Snapshot',
+          'api-reference/core/useRecoilTransactionObserver',
+          'api-reference/core/useRecoilSnapshot',
+          'api-reference/core/useGotoRecoilSnapshot',
+        ],
+        Misc: [
+          'api-reference/core/useRecoilBridgeAcrossReactRoots',
         ],
       },
     ],
