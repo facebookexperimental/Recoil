414a1d2d831a14d24176dec23bfcde6a1fab3ede
Aaron Abramov <dabramov@fb.com>
2021-01-19 14:50:31 -0800

    eslint rule to warn on `@fb-only` and `@oss-only` comments (#844)
    Summary:
    I'm trying to investigate how to better decouple Recoil from internal infra, and the first step is figuring out where we execute the code conditionally depending on whether we're running it at FB or not.
    
    This lint rule highlights all usages of `fb-only` and `oss-only`. It is disabled for now, but i'll be using it to identify and move out all these forks.
    
    ![Screen Shot 2021-01-19 at 3 37 39 PM](https://user-images.githubusercontent.com/940133/105096664-0b276b80-5a6d-11eb-9250-701d197f75d9.png)
    ![Screen Shot 2021-01-19 at 3 37 45 PM](https://user-images.githubusercontent.com/940133/105096668-0e225c00-5a6d-11eb-98f8-ec4dbea15aea.png)
    
    Pull Request resolved: https://github.com/facebookexperimental/Recoil/pull/844
    
    Reviewed By: drarmstr
    
    Differential Revision: D25961057
    
    Pulled By: aaronabramov
    
    fbshipit-source-id: 30ae1d4d025f0f9818eecf3fb8a0cb86c373286f

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,18 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+ *
+ * @format
  */
+
+const rulesDirPlugin = require('eslint-plugin-rulesdir');
+const path = require('path');
 
 const OFF = 0;
 const WARNING = 1;
 const ERROR = 2;
+
+rulesDirPlugin.RULES_DIR = path.resolve(__dirname, './eslint-rules');
 
 module.exports = {
   env: {
@@ -23,12 +30,13 @@ module.exports = {
     ecmaVersion: 11,
     sourceType: 'module',
   },
-  plugins: ['flowtype', 'react', 'jest', 'fb-www'],
+  plugins: ['flowtype', 'react', 'jest', 'fb-www', 'rulesdir'],
   rules: {
     strict: 0,
-    'jsx-a11y/href-no-hash': 'off',
-    'react/jsx-key': 'off',
-    'react/prop-types': 'off',
+    'jsx-a11y/href-no-hash': OFF,
+    'react/jsx-key': OFF,
+    'react/prop-types': OFF,
+    'rulesdir/no-fb-only': OFF,
   },
   settings: {
     react: {
