3af0e84172d98cc46dc4254fef6b5eafd4981499
Douglas Armstrong <dougarmstrong@fb.com>
2021-09-24 15:03:46 -0700

    Refactor tests to avoid URL conflicts
    Summary:
    Refactor tests so the tests which subscribe to listening to URL changes are pulled out in a separate module.  This allows us to have full coverage for testing using different parts of the URL (i.e. path, anchor hash, query search params) without causing the listening tests to trip up when other tests change the global URL in incompatible ways.
    
    Also, normalize the validation and serialization mock-ups.
    
    Differential Revision: D30947076
    
    fbshipit-source-id: 80bcbb412d4b736fad6e82bfbbcffb975e7afbfc

---
+++ src/contrib/recoil-sync/__test_utils__/recoil-url-sync_mockSerialization.js
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+ *
+ * @emails oncall+recoil
+ * @flow strict-local
+ * @format
+ */
+'use strict';
+
+// TODO UPDATE IMPORTS TO USE PUBLIC INTERFACE
+
+import type {LocationOption} from '../recoil-url-sync';
+
+const {
+  flushPromisesAndTimers,
+} = require('../../../testing/Recoil_TestingUtils');
+const {useRecoilURLSync} = require('../recoil-url-sync');
+const React = require('react');
+
+// ////////////////////////////
+// // Mock Serialization
+// ////////////////////////////
+// Object.fromEntries() is not available in GitHub's version of Node.js (9/21/2021)
+const mapToObj = map => {
+  const obj = {};
+  for (const [key, value] of map.entries()) {
+    obj[key] = value;
+  }
+  return obj;
+};
+function TestURLSync({
+  syncKey,
+  location,
+}: {
+  syncKey?: string,
+  location: LocationOption,
+}): React.Node {
+  useRecoilURLSync({
+    syncKey,
+    location,
+    serialize: items =>
+      `${location.part === 'href' ? '/TEST#' : ''}${encodeURI(
+        JSON.stringify(mapToObj(items)),
+      )}`,
+    deserialize: str => {
+      const stateStr = location.part === 'href' ? str.split('#')[1] : str;
+      // Skip the default URL parts which don't conform to the serialized standard
+      if (
+        stateStr == null ||
+        stateStr === 'anchor' ||
+        stateStr === 'foo=bar' ||
+        stateStr === 'bar'
+      ) {
+        return new Map();
+      }
+      try {
+        return new Map(Object.entries(JSON.parse(decodeURI(stateStr))));
+      } catch (e) {
+        console.error('Error parsing: ', location, decodeURI(stateStr));
+        throw e;
+      }
+    },
+  });
+  return null;
+}
+
+function encodeState(obj) {
+  return `${encodeURI(JSON.stringify(obj))}`;
+}
+
+function encodeURLPart(href: string, loc: LocationOption, obj): string {
+  const encoded = encodeState(obj);
+  const url = new URL(href);
+  switch (loc.part) {
+    case 'href':
+      url.pathname = '/TEST';
+      url.hash = encoded;
+      break;
+    case 'hash':
+      url.hash = encoded;
+      break;
+    case 'search': {
+      const {queryParam} = loc;
+      if (queryParam == null) {
+        url.search = encoded;
+      } else {
+        const searchParams = url.searchParams;
+        searchParams.set(queryParam, encoded);
+        url.search = searchParams.toString();
+      }
+      break;
+    }
+  }
+  return url.href;
+}
+
+function encodeURL(parts: Array<[LocationOption, {...}]>): string {
+  let href = window.location.href;
+  for (const [loc, obj] of parts) {
+    href = encodeURLPart(href, loc, obj);
+  }
+  return href;
+}
+
+function expectURL(parts: Array<[LocationOption, {...}]>) {
+  expect(window.location.href).toBe(encodeURL(parts));
+}
+
+async function gotoURL(parts: Array<[LocationOption, {...}]>) {
+  history.pushState(null, '', encodeURL(parts));
+  history.pushState(null, '', '/POPSTATE');
+  history.back();
+  await flushPromisesAndTimers();
+}
+
+module.exports = {
+  TestURLSync,
+  encodeURL,
+  expectURL,
+  gotoURL,
+};
