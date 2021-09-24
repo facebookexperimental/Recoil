6b221e41861cb70b372d1c83670fcbae27d33832
Douglas Armstrong <dougarmstrong@fb.com>
2021-09-24 16:29:26 -0700

    Refactor tests to avoid URL conflicts (#1246)
    Summary:
    Pull Request resolved: https://github.com/facebookexperimental/Recoil/pull/1246
    
    Refactor tests so the tests which subscribe to listening to URL changes are pulled out in a separate module.  This allows us to have full coverage for testing using different parts of the URL (i.e. path, anchor hash, query search params) without causing the listening tests to trip up when other tests change the global URL in incompatible ways.
    
    Also, normalize the validation and serialization mock-ups.
    
    Reviewed By: davidmccabe
    
    Differential Revision: D30947076
    
    fbshipit-source-id: 1e01374179d02bf1a1a139856f83db7fdcdc08e7

--- src/contrib/recoil-sync/__tests__/recoil-url-sync-test.js
+++ src/contrib/recoil-sync/__tests__/recoil-url-sync-test.js
@@ -8,14 +8,20 @@
 'use strict';
 
 // TODO UPDATE IMPORTS TO USE PUBLIC INTERFACE
-// TODO PUBLIC LOADABLE INTERFACE
-
-import type {Loadable} from '../../../adt/Recoil_Loadable';
-import type {LocationOption} from '../recoil-url-sync';
 
 const {act} = require('ReactTestUtils');
 
-const {loadableWithValue} = require('../../../adt/Recoil_Loadable');
+const {
+  upgrade,
+  validateAny,
+  validateNumber,
+  validateString,
+} = require('../__test_utils__/recoil-sync_mockValidation');
+const {
+  TestURLSync,
+  encodeURL,
+  expectURL,
+} = require('../__test_utils__/recoil-url-sync_mockSerialization');
 const atom = require('../../../recoil_values/Recoil_atom');
 const {
   ReadsAtom,
@@ -24,126 +30,12 @@ const {
   renderElements,
 } = require('../../../testing/Recoil_TestingUtils');
 const {syncEffect} = require('../recoil-sync');
-const {urlSyncEffect, useRecoilURLSync} = require('../recoil-url-sync');
+const {urlSyncEffect} = require('../recoil-url-sync');
 const React = require('react');
 
 let atomIndex = 0;
 const nextKey = () => `recoil-url-sync/${atomIndex++}`;
 
-////////////////////////////
-// Mock validation library
-////////////////////////////
-const validateAny = loadableWithValue;
-const validateString = x =>
-  typeof x === 'string' ? loadableWithValue(x) : null;
-const validateNumber = x =>
-  typeof x === 'number' ? loadableWithValue(x) : null;
-function upgrade<From, To>(
-  validate: mixed => ?Loadable<From>,
-  upgrader: From => To,
-): mixed => ?Loadable<To> {
-  return x => validate(x)?.map(upgrader);
-}
-
-// ////////////////////////////
-// // Mock Serialization
-// ////////////////////////////
-// Object.fromEntries() is not available in GitHub's version of Node.js (9/21/2021)
-const mapToObj = map => {
-  const obj = {};
-  for (const [key, value] of map.entries()) {
-    obj[key] = value;
-  }
-  return obj;
-};
-function TestURLSync({
-  syncKey,
-  location,
-}: {
-  syncKey?: string,
-  location: LocationOption,
-}) {
-  useRecoilURLSync({
-    syncKey,
-    location,
-    serialize: items =>
-      `${location.part === 'href' ? '/TEST#' : ''}${encodeURI(
-        JSON.stringify(mapToObj(items)),
-      )}`,
-    deserialize: str => {
-      const stateStr = location.part === 'href' ? str.split('#')[1] : str;
-      // Skip the default URL parts which don't conform to the serialized standard
-      if (
-        stateStr == null ||
-        stateStr === 'anchor' ||
-        stateStr === 'foo=bar' ||
-        stateStr === 'bar'
-      ) {
-        return new Map();
-      }
-      try {
-        return new Map(Object.entries(JSON.parse(decodeURI(stateStr))));
-      } catch (e) {
-        console.error('Error parsing: ', location, decodeURI(stateStr));
-        throw e;
-      }
-    },
-  });
-  return null;
-}
-
-function encodeState(obj) {
-  return `${encodeURI(JSON.stringify(obj))}`;
-}
-
-function encodeURLPart(href: string, loc: LocationOption, obj): string {
-  const encoded = encodeState(obj);
-  const url = new URL(href);
-  switch (loc.part) {
-    case 'href':
-      url.pathname = '/TEST';
-      url.hash = encoded;
-      break;
-    case 'hash':
-      url.hash = encoded;
-      break;
-    case 'search': {
-      const {queryParam} = loc;
-      if (queryParam == null) {
-        url.search = encoded;
-      } else {
-        const searchParams = url.searchParams;
-        searchParams.set(queryParam, encoded);
-        url.search = searchParams.toString();
-      }
-      break;
-    }
-  }
-  return url.href;
-}
-
-function encodeURL(parts: Array<[LocationOption, {...}]>) {
-  let href = window.location.href;
-  for (const [loc, obj] of parts) {
-    href = encodeURLPart(href, loc, obj);
-  }
-  return href;
-}
-
-function expectURL(parts: Array<[LocationOption, {...}]>) {
-  expect(window.location.href).toBe(encodeURL(parts));
-}
-
-async function gotoURL(parts: Array<[LocationOption, {...}]>) {
-  history.pushState(null, '', encodeURL(parts));
-  history.pushState(null, '', '/POPSTATE');
-  history.back();
-  await flushPromisesAndTimers();
-}
-
-///////////////////////
-// Tests
-///////////////////////
 describe('Test URL Persistence', () => {
   beforeEach(() => {
     history.replaceState(null, '', '/path/page.html?foo=bar#anchor');
@@ -193,15 +85,20 @@ describe('Test URL Persistence', () => {
     remainder();
   }
 
-  // TODO Re-enable test when supporting multiple <RecoilRoot>'s
-  // test('Write to URL', () =>
-  //   testWriteToURL({part: 'href'}, () => {
-  //     expect(location.search).toBe('');
-  //     expect(location.pathname).toBe('/TEST');
-  //   }));
+  test('Write to URL', () =>
+    testWriteToURL({part: 'href'}, () => {
+      expect(location.search).toBe('');
+      expect(location.pathname).toBe('/TEST');
+    }));
   test('Write to URL - Anchor Hash', () =>
     testWriteToURL({part: 'hash'}, () => {
       expect(location.search).toBe('?foo=bar');
+    }));
+  test('Write to URL - Query Search', () =>
+    testWriteToURL({part: 'search'}, () => {
+      expect(location.hash).toBe('#anchor');
+      expect(new URL(location.href).searchParams.get('foo')).toBe(null);
+      expect(new URL(location.href).searchParams.get('bar')).toBe(null);
     }));
   test('Write to URL - Query Search Param', () =>
     testWriteToURL({part: 'search', queryParam: 'bar'}, () => {
@@ -289,9 +186,9 @@ describe('Test URL Persistence', () => {
     expect(container.textContent).toBe('"A""B""DEFAULT"');
   }
 
-  // TODO Re-enable test when supporting multiple <RecoilRoot>'s
-  // test('Read from URL', () => testReadFromURL({part: 'href'}));
+  test('Read from URL', () => testReadFromURL({part: 'href'}));
   test('Read from URL - Anchor Hash', () => testReadFromURL({part: 'hash'}));
+  test('Read from URL - Search Query', () => testReadFromURL({part: 'search'}));
   test('Read from URL - Search Query Param', () =>
     testReadFromURL({part: 'search', queryParam: 'param'}));
   test('Read from URL - Search Query Param with other param', () =>
@@ -306,7 +203,7 @@ describe('Test URL Persistence', () => {
       default: 'DEFAULT',
       effects_UNSTABLE: [
         // No matching sync effect
-        syncEffect({restore: validateString}),
+        syncEffect<string>({restore: validateString}),
       ],
     });
 
@@ -317,7 +214,9 @@ describe('Test URL Persistence', () => {
       effects_UNSTABLE: [
         // This sync effect is ignored
         syncEffect<string>({restore: upgrade(validateString, () => 'IGNORE')}),
-        syncEffect<string>({restore: upgrade(validateNumber, num => `${num}`)}),
+        syncEffect<string>({
+          restore: upgrade<number, string>(validateNumber, num => `${num}`),
+        }),
         // This sync effect is ignored
         syncEffect<string>({restore: upgrade(validateString, () => 'IGNORE')}),
       ],
@@ -373,8 +272,10 @@ describe('Test URL Persistence', () => {
       key: 'recoil-url-sync read/write upgrade type',
       default: 'DEFAULT',
       effects_UNSTABLE: [
-        syncEffect<string>({restore: upgrade(validateNumber, num => `${num}`)}),
-        syncEffect({restore: validateString}),
+        syncEffect<string>({
+          restore: upgrade<number, string>(validateNumber, num => `${num}`),
+        }),
+        syncEffect<string>({restore: validateString}),
       ],
     });
     const atomB = atom({
@@ -460,245 +361,6 @@ describe('Test URL Persistence', () => {
       [loc1, {}],
       [loc2, {}],
     ]);
-  });
-
-  test('Listen to URL changes', async () => {
-    const locFoo = {part: 'search', queryParam: 'foo'};
-    const locBar = {part: 'search', queryParam: 'bar'};
-
-    const atomA = atom({
-      key: 'recoil-url-sync listen',
-      default: 'DEFAULT',
-      effects_UNSTABLE: [syncEffect({syncKey: 'foo', restore: validateAny})],
-    });
-    const atomB = atom({
-      key: 'recoil-url-sync listen to multiple keys',
-      default: 'DEFAULT',
-      effects_UNSTABLE: [
-        syncEffect({syncKey: 'foo', key: 'KEY A', restore: validateAny}),
-        syncEffect({syncKey: 'foo', key: 'KEY B', restore: validateAny}),
-      ],
-    });
-    const atomC = atom({
-      key: 'recoil-url-sync listen to multiple storage',
-      default: 'DEFAULT',
-      effects_UNSTABLE: [
-        syncEffect({syncKey: 'foo', restore: validateAny}),
-        syncEffect({syncKey: 'bar', restore: validateAny}),
-      ],
-    });
-
-    history.replaceState(
-      null,
-      '',
-      encodeURL([
-        [
-          locFoo,
-          {
-            'recoil-url-sync listen': 'A',
-            'KEY A': 'B',
-            'recoil-url-sync listen to multiple storage': 'C',
-          },
-        ],
-      ]),
-    );
-
-    const container = renderElements(
-      <>
-        <TestURLSync syncKey="foo" location={locFoo} />
-        <TestURLSync syncKey="bar" location={locBar} />
-        <ReadsAtom atom={atomA} />
-        <ReadsAtom atom={atomB} />
-        <ReadsAtom atom={atomC} />
-      </>,
-    );
-
-    // Initial load will use the fallback storage for C
-    expect(container.textContent).toBe('"A""B""C"');
-    expectURL([
-      [
-        locFoo,
-        {
-          'recoil-url-sync listen': 'A',
-          'KEY A': 'B',
-          'recoil-url-sync listen to multiple storage': 'C',
-        },
-      ],
-    ]);
-
-    // Subscribe to new value
-    act(() =>
-      gotoURL([
-        [
-          locFoo,
-          {
-            'recoil-url-sync listen': 'AA',
-            'KEY A': 'B',
-            'recoil-url-sync listen to multiple storage': 'C1',
-          },
-        ],
-      ]),
-    );
-    expect(container.textContent).toBe('"AA""B""C1"');
-    // Changing value of C caused it to sync with locBar
-    expectURL([
-      [
-        locFoo,
-        {
-          'recoil-url-sync listen': 'AA',
-          'KEY A': 'B',
-          'recoil-url-sync listen to multiple storage': 'C1',
-        },
-      ],
-      [
-        locBar,
-        {
-          'recoil-url-sync listen to multiple storage': 'C1',
-        },
-      ],
-    ]);
-
-    // Subscribe to new value from different key
-    act(() =>
-      gotoURL([
-        [
-          locFoo,
-          {
-            'recoil-url-sync listen': 'AA',
-            'KEY A': 'BB',
-            'recoil-url-sync listen to multiple storage': 'C1',
-          },
-        ],
-      ]),
-    );
-    expectURL([
-      [
-        locFoo,
-        {
-          'recoil-url-sync listen': 'AA',
-          'KEY A': 'BB',
-          'recoil-url-sync listen to multiple storage': 'C1',
-        },
-      ],
-      [
-        locBar,
-        {
-          'recoil-url-sync listen to multiple storage': 'C1',
-        },
-      ],
-    ]);
-    expect(container.textContent).toBe('"AA""BB""C1"');
-    act(() =>
-      gotoURL([
-        [
-          locFoo,
-          {
-            'recoil-url-sync listen': 'AA',
-            'KEY A': 'BB',
-            'KEY B': 'BBB',
-            'recoil-url-sync listen to multiple storage': 'C1',
-          },
-        ],
-      ]),
-    );
-    expect(container.textContent).toBe('"AA""BBB""C1"');
-    act(() =>
-      gotoURL([
-        [
-          locFoo,
-          {
-            'recoil-url-sync listen': 'AA',
-            'KEY A': 'IGNORE',
-            'KEY B': 'BBB',
-            'recoil-url-sync listen to multiple storage': 'C1',
-          },
-        ],
-      ]),
-    );
-    expect(container.textContent).toBe('"AA""BBB""C1"');
-    act(() =>
-      gotoURL([
-        [
-          locFoo,
-          {
-            'recoil-url-sync listen': 'AA',
-            'KEY A': 'BBBB',
-            'recoil-url-sync listen to multiple storage': 'C1',
-          },
-        ],
-      ]),
-    );
-    expect(container.textContent).toBe('"AA""BBBB""C1"');
-
-    // Subscribe to reset
-    act(() =>
-      gotoURL([
-        [
-          locFoo,
-          {
-            'recoil-url-sync listen to multiple storage': 'C1',
-          },
-        ],
-      ]),
-    );
-    expect(container.textContent).toBe('"DEFAULT""DEFAULT""C1"');
-
-    // Subscribe to new value from different storage
-    act(() =>
-      gotoURL([
-        [
-          locFoo,
-          {
-            'recoil-url-sync listen': 'AA',
-            'KEY A': 'B',
-            'recoil-url-sync listen to multiple storage': 'C1',
-          },
-        ],
-        [locBar, {}],
-      ]),
-    );
-    expect(container.textContent).toBe('"AA""B""DEFAULT"');
-    act(() =>
-      gotoURL([
-        [
-          locFoo,
-          {
-            'recoil-url-sync listen to multiple storage': 'C1',
-          },
-        ],
-        [
-          locBar,
-          {
-            'recoil-url-sync listen to multiple storage': 'CC2',
-          },
-        ],
-      ]),
-    );
-    expect(container.textContent).toBe('"DEFAULT""DEFAULT""CC2"');
-    act(() =>
-      gotoURL([
-        [
-          locFoo,
-          {
-            'recoil-url-sync listen to multiple storage': 'C1',
-          },
-        ],
-        [locBar, {}],
-      ]),
-    );
-    expect(container.textContent).toBe('"DEFAULT""DEFAULT""DEFAULT"');
-    act(() =>
-      gotoURL([
-        [
-          locFoo,
-          {
-            'recoil-url-sync listen to multiple storage': 'CC1',
-          },
-        ],
-        [locBar, {}],
-      ]),
-    );
-    expect(container.textContent).toBe('"DEFAULT""DEFAULT""DEFAULT"');
   });
 
   test('Persist default on read', async () => {
