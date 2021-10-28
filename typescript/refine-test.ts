563ec91c1ee3f2e55180ade39b9e366bca165c9b
Ben Southgate <bsouthga@fb.com>
2021-10-28 14:05:00 -0700

    refine typescript types (#1334)
    Summary:
    Pull Request resolved: https://github.com/facebookexperimental/Recoil/pull/1334
    
    - started typescript versions of the refine types
    
    typescript can actually describe refine better than flow, for example:
    
    {F672515126}
    
    Reviewed By: drarmstr
    
    Differential Revision: D31871619
    
    fbshipit-source-id: 060d10f6753010a40a1a7e30ea31c72cb6a8784f

---
+++ typescript/refine-test.ts
@@ -0,0 +1,232 @@
+// Minimum TypeScript Version: 3.7
+
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails oncall+recoil
+ */
+
+import {
+  Get,
+  boolean,
+  stringLiterals,
+  string,
+  number,
+  mixed,
+  nullable,
+  voidable,
+  date,
+  custom,
+  asType,
+  match,
+  union,
+  or,
+  object,
+  dict,
+  array,
+  tuple,
+  map,
+  set,
+  writableObject,
+  writableArray,
+  writableDict,
+  optional,
+} from './refine';
+
+// turn of lint for unused test vars
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/**
+ *
+ * utility type tests
+ *
+ */
+{
+  const checker = object({a: number()});
+  type MyType = Get<typeof checker>;
+  const x: MyType = {
+    a: 1,
+  }
+
+  const y: MyType = {
+    a: 'test', // $ExpectError
+  }
+}
+
+/**
+ *
+ * primitive tests
+ *
+ */
+{
+  const rboolean = boolean()({});
+  if (rboolean.type == 'success') {
+    const v: boolean = rboolean.value;
+  }
+
+  const rstring = string()({});
+  if (rstring.type == 'success') {
+    const v: string = rstring.value;
+  }
+
+  const rnumber = number()({});
+  if (rnumber.type == 'success') {
+    const v: number = rnumber.value;
+  }
+
+  const runknown = mixed()({});
+  if (runknown.type == 'success') {
+    const v: unknown = runknown.value;
+  }
+
+  const rliterals = stringLiterals<'a' | 'b'>({a: 'a', b: 'b'})({});
+  if (rliterals.type == 'success') {
+    const v: 'a' | 'b' = rliterals.value;
+  }
+
+  const rsorn = or(string(), number())({});
+  if (rsorn.type == 'success') {
+    const v: string | number = rsorn.value;
+  }
+
+  const rsunionn = union(string(), number(), boolean())({});
+  if (rsunionn.type == 'success') {
+    const v: string | number | boolean = rsunionn.value;
+  }
+
+  const rvoidablestring = voidable(string())({});
+  if (rvoidablestring.type == 'success') {
+    const v: void | string = rvoidablestring.value;
+    const x: string = rvoidablestring.value; // $ExpectError
+    const z: string | null = rvoidablestring.value; // $ExpectError
+  }
+
+  const rnullable = nullable(string())({});
+  if (rnullable.type == 'success') {
+    const v: null | string | void = rnullable.value;
+    const x: string = rnullable.value; // $ExpectError
+    const z: string | void = rnullable.value; // $ExpectError
+  }
+
+  const rdate = date()({});
+  if (rdate.type == 'success') {
+    const v: Date = rdate.value;
+  }
+}
+
+/**
+ *
+ * collection tests
+ *
+ */
+{
+  const rarray = array(string())({});
+  if (rarray.type === 'success') {
+    const s: string = rarray.value[0];
+    rarray.value.push('test'); // $ExpectError
+  }
+
+  const rwarray = writableArray(string())({});
+  if (rwarray.type === 'success') {
+    const s: string = rwarray.value[0];
+    rwarray.value.push('test');
+  }
+}
+
+{
+  const check = object({a: optional(string()), b: string()});
+
+  type ObjectWithOptional = Get<typeof check>;
+
+  const r1: ObjectWithOptional = {
+    b: 'test',
+  };
+
+  const r2: ObjectWithOptional = {
+    b: 'test',
+    a: 'test',
+  };
+
+  const result = check({});
+
+  if (result.type === 'success') {
+    result.value.b.includes('test');
+  } else {
+    result.message.includes('');
+  }
+
+  const rwobject = writableObject({c: number()})({});
+  if (rwobject.type == 'success') {
+    rwobject.value.c = 1;
+    const v: {c: number} = rwobject.value;
+  }
+}
+
+{
+  const rdict = dict(string())({});
+  if (rdict.type == 'success') {
+    const v: {readonly [key: string]: string} = rdict.value;
+  }
+
+  const rwdict = writableDict(number())({});
+  if (rwdict.type == 'success') {
+    rwdict.value.key = 1;
+    const v: {[key: string]: number} = rwdict.value;
+  }
+}
+
+{
+  const rtuple = tuple(string(), number())({});
+  if (rtuple.type === 'success') {
+    const v: readonly [string, number] = rtuple.value;
+  }
+}
+
+{
+  const rmap = map(array(string()), dict(number()))({});
+  if (rmap.type === 'success') {
+    const v: ReadonlyMap<readonly string[], Readonly<{[key: string]: number}>> = rmap.value;
+  }
+}
+
+{
+  const rset = set(date())({});
+  if (rset.type === 'success') {
+    const v: ReadonlySet<Date> = rset.value;
+  }
+}
+
+
+/**
+ *
+ * utilities
+ *
+ */
+{
+  const rasnum = asType(string(), s => parseInt(s, 10))({});
+  if (rasnum.type == 'success') {
+    const v: number = rasnum.value;
+  }
+
+  const rmatch = match(
+    asType(number(), n => n.toString()),
+    asType(boolean(), b => b.toString()),
+    string()
+  )({});
+
+  if (rmatch.type == 'success') {
+    const v: string = rmatch.value;
+  }
+
+  class Custom {}
+
+  const isCustomClass = custom(value => value instanceof Custom ? value : null);
+  const rcustomclass = isCustomClass({});
+
+  if (rcustomclass.type == 'success') {
+    const v: Custom = rcustomclass.value;
+  }
+}
---
+++ typescript/refine.d.ts
@@ -0,0 +1,594 @@
+// Minimum TypeScript Version: 3.7
+
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails oncall+recoil
+ */
+
+type CheckerResult<V> = V extends Checker<infer Result>
+  ? Result
+  : V extends OptionalPropertyChecker<infer OptionalResult>
+  ? OptionalResult
+  : never;
+
+export type Get<V> = V extends Checker<infer Result> ? Result : never;
+
+/**
+ * This file is a manual translation of the flow types, which are the source of truth, so we should not introduce new terminology or behavior in this file.
+ */
+
+export declare class Path {
+  constructor(parent?: Path | null, field?: string);
+  extend(field: string): Path;
+  toString(): string;
+}
+
+/**
+ * the result of failing to match a value to its expected type
+ */
+export type CheckFailure = Readonly<{
+  type: 'failure';
+  message: string;
+  path: Path;
+}>;
+
+/**
+ * the result of successfully matching a value to its expected type
+ */
+export type CheckSuccess<V> = Readonly<{
+  type: 'success';
+  value: V;
+  // if using `nullable` with the `nullWithWarningWhenInvalid` option,
+  // failures will be appended here
+  warnings: ReadonlyArray<CheckFailure>;
+}>;
+
+/**
+ * the result of checking whether a type matches an expected value
+ */
+export type CheckResult<V> = CheckSuccess<V> | CheckFailure;
+
+/**
+ * a function which checks if a given mixed value matches a type V,
+ * returning the value if it does, otherwise a failure message.
+ */
+export type Checker<V> = (value: unknown, path?: Path) => CheckResult<V>;
+
+/**
+ * wrap value in an object signifying successful checking
+ */
+export declare function success<V>(
+  value: V,
+  warnings: ReadonlyArray<CheckFailure>,
+): CheckSuccess<V>;
+
+/**
+ * indicate typecheck failed
+ */
+export declare function failure(message: string, path: Path): CheckFailure;
+
+/**
+ * utility function for composing checkers
+ */
+export declare function compose<T, V>(
+  checker: Checker<T>,
+  next: (success: CheckSuccess<T>, path: Path) => CheckResult<V>,
+): Checker<V>;
+
+/**
+ * function to assert that a given value matches a checker
+ */
+export type AssertionFunction<V> = (value: unknown) => V;
+
+/**
+ * function to coerce a given value to a checker type, returning null if invalid
+ */
+export type CoercionFunction<V> = (value: unknown) => V | null | void;
+
+/**
+ * create a function to assert a value matches a checker, throwing otherwise
+ *
+ * For example,
+ *
+ * ```
+ * const assert = assertion(array(number()));
+ * const value: Array<number> = assert([1,2]);
+ *
+ * try {
+ *   // should throw with `Refine.js assertion failed: ...`
+ *   const invalid = assert('test');
+ * } catch {
+ * }
+ * ```
+ */
+export declare function assertion<T>(
+  checker: Checker<T>,
+  errorMessage?: string,
+): AssertionFunction<T>;
+
+/**
+ * create a CoercionFunction given a checker.
+ *
+ * Allows for null-coercing a value to a given type using a checker. Optionally
+ * provide a callback which receives the full check
+ * result object (e.g. for logging).
+ *
+ * Example:
+ *
+ * ```javascript
+ * import {coercion, record, string} from 'refine';
+ * import MyLogger from './MyLogger';
+ *
+ * const Person = record({
+ *   name: string(),
+ *   hobby: string(),
+ * });
+ *
+ * const coerce = coercion(Person, result => MyLogger.log(result));
+ *
+ * declare value: mixed;
+ *
+ * // ?Person
+ * const person = coerce(value);
+ * ```
+ */
+export declare function coercion<T>(
+  checker: Checker<T>,
+  onResult?: (checker: CheckResult<T>) => void,
+): CoercionFunction<T>;
+
+/**
+ * checker to assert if a mixed value is an array of
+ * values determined by a provided checker
+ */
+export declare function array<V>(
+  valueChecker: Checker<V>,
+): Checker<ReadonlyArray<V>>;
+
+/**
+ * checker to assert if a mixed value is a tuple of values
+ * determined by provided checkers. Extra entries are ignored.
+ *
+ * Example:
+ * ```jsx
+ * const checker = tuple( number(), string() );
+ * ```
+ *
+ * Example with optional trailing entry:
+ * ```jsx
+ * const checker = tuple( number(), voidable(string()));
+ * ```
+ */
+export declare function tuple<Checkers extends [...unknown[]]>(
+  ...checkers: Checkers
+): Checker<Readonly<{[K in keyof Checkers]: CheckerResult<Checkers[K]>}>>;
+
+/**
+ * checker to assert if a mixed value is a string-keyed dict of
+ * values determined by a provided checker
+ */
+export declare function dict<V>(
+  valueChecker: Checker<V>,
+): Checker<Readonly<{[key: string]: V}>>;
+
+
+
+// expose opaque version of optional property as public api,
+// forcing consistent usage of built-in `optional` to define optional properties
+declare const __opaque: unique symbol;
+export type OptionalPropertyChecker<T> = {
+  readonly [__opaque]: T;
+}
+
+/**
+ * checker which can only be used with `object` or `writablObject`. Marks a
+ * field as optional, skipping the key in the result if it doesn't
+ * exist in the input.
+ *
+ * @example
+ * ```jsx
+ * import {object, string, optional} from 'refine';
+ *
+ * const checker = object({a: string(), b: optional(string())});
+ * assert(checker({a: 1}).type === 'success');
+ * ```
+ */
+declare function optional<T>(checker: Checker<T>): OptionalPropertyChecker<T>;
+
+type CheckerObject = Readonly<{
+  [key: string]: Checker<unknown> | OptionalPropertyChecker<unknown>;
+}>;
+
+type CheckersToValues<Checkers extends CheckerObject> = {
+  [K in keyof Checkers]: CheckerResult<Checkers[K]>;
+}
+
+type WhereValue<Checkers extends CheckerObject, Condition> = Pick<Checkers, {
+  [Key in keyof Checkers]: Checkers[Key] extends Condition ? Key : never
+}[keyof Checkers]>;
+
+type RequiredCheckerProperties<Checkers extends CheckerObject> = WhereValue<
+  Checkers,
+  Checker<unknown>
+>;
+
+type OptionalCheckerProperties<Checkers extends CheckerObject> = Partial<
+  WhereValue<Checkers, OptionalPropertyChecker<unknown>>
+>;
+
+type ObjectCheckerResult<Checkers extends CheckerObject> =
+  CheckersToValues<RequiredCheckerProperties<Checkers> & OptionalCheckerProperties<Checkers>>;
+
+/**
+ * checker to assert if a mixed value is a fixed-property object,
+ * with key-value pairs determined by a provided object of checkers.
+ * Any extra properties in the input object values are ignored.
+ * Class instances are not supported, use the custom() checker for those.
+ *
+ * Example:
+ * ```jsx
+ * const myObject = object({
+ *   name: string(),
+ *   job: object({
+ *     years: number(),
+ *     title: string(),
+ *   }),
+ * });
+ * ```
+ *
+ * Properties can be optional using `voidable()` or have default values
+ * using `withDefault()`:
+ * ```jsx
+ * const customer = object({
+ *   name: string(),
+ *   reference: voidable(string()),
+ *   method: withDefault(string(), 'email'),
+ * });
+ * ```
+ */
+export declare function object<Checkers extends CheckerObject>(
+  checkers: Checkers
+): Checker<Readonly<ObjectCheckerResult<Checkers>>>;
+
+/**
+ * checker to assert if a mixed value is a Set type
+ */
+export declare function set<T>(checker: Checker<T>): Checker<ReadonlySet<T>>;
+
+/**
+ * checker to assert if a mixed value is a Map.
+ */
+export declare function map<K, V>(
+  keyChecker: Checker<K>,
+  valueChecker: Checker<V>,
+): Checker<ReadonlyMap<K, V>>;
+
+/**
+ * identical to `array()` except the resulting value is a writable flow type.
+ */
+export declare function writableArray<V>(
+  valueChecker: Checker<V>,
+): Checker<Array<V>>;
+
+/**
+ * identical to `dict()` except the resulting value is a writable flow type.
+ */
+export declare function writableDict<V>(
+  valueChecker: Checker<V>,
+): Checker<{[key: string]: V}>;
+
+/**
+ * identical to `object()` except the resulting value is a writable flow type.
+ */
+export declare function writableObject<
+  Checkers extends CheckerObject,
+>(
+  checkers: Checkers,
+): Checker<ObjectCheckerResult<Checkers>>;
+
+/**
+ * function which takes a json string, parses it,
+ * and matches it with a checker (returning null if no match)
+ */
+export type JSONParser<T> = (input: string | null | void) => T;
+
+/**
+ * creates a JSON parser which will error if the resulting value is invalid
+ */
+export declare function jsonParserEnforced<T>(
+  checker: Checker<T>,
+  suffix?: string,
+): JSONParser<T>;
+
+/**
+ * convienience function to wrap a checker in a function
+ * for easy JSON string parsing.
+ */
+export declare function jsonParser<T>(
+  checker: Checker<T>,
+): JSONParser<T | void | null>;
+
+/**
+ * a mixed (i.e. untyped) value
+ */
+export declare function mixed(): Checker<unknown>;
+
+/**
+ * checker to assert if a mixed value matches a literal value
+ */
+export declare function literal<
+  T extends string | boolean | number | null | void,
+>(literalValue: T): Checker<T>;
+
+/**
+ * boolean value checker
+ */
+export declare function boolean(): Checker<boolean>;
+
+/**
+ * checker to assert if a mixed value is a number
+ */
+export declare function number(): Checker<number>;
+
+/**
+ * Checker to assert if a mixed value is a string.
+ *
+ * Provide an optional RegExp template to match string against.
+ */
+export declare function string(regex?: RegExp): Checker<string>;
+
+/**
+ * checker to assert if a mixed value matches a union of string literals.
+ * Legal values are provided as keys in an object and may be translated by
+ * providing values in the object.
+ *
+ * For example:
+ * ```jsx
+ * ```
+ */
+export declare function stringLiterals<T>(enumValues: {
+  readonly [key: string]: T;
+}): Checker<T>;
+
+/**
+ * checker to assert if a mixed value is a Date object
+ */
+export declare function date(): Checker<Date>;
+
+/**
+ * Cast the type of a value after passing a given checker
+ *
+ * For example:
+ *
+ * ```javascript
+ * import {string, asType} from 'refine';
+ *
+ * opaque type ID = string;
+ *
+ * const IDChecker: Checker<ID> = asType(string(), s => (s: ID));
+ * ```
+ */
+export declare function asType<A, B>(
+  checker: Checker<A>,
+  cast: (input: A) => B,
+): Checker<B>;
+
+/**
+ * checker which asserts the value matches
+ * at least one of the two provided checkers
+ */
+export declare function or<A, B>(
+  aChecker: Checker<A>,
+  bChecker: Checker<B>,
+): Checker<A | B>;
+
+type ElementType<T> = T extends unknown[] ? T[number] : T;
+
+/**
+ * checker which asserts the value matches
+ * at least one of the provided checkers
+ */
+export declare function union<Checkers extends ReadonlyArray<Checker<unknown>>>(
+  ...checkers: Checkers
+): Checker<ElementType<{[K in keyof Checkers]: CheckerResult<Checkers[K]>}>>;
+
+/**
+ * Provide a set of checkers to check in sequence to use the first match.
+ * This is similar to union(), but all checkers must have the same type.
+ *
+ * This can be helpful for supporting backward compatibility.  For example the
+ * following loads a string type, but can also convert from a number as the
+ * previous version or pull from an object as an even older version:
+ *
+ * ```jsx
+ * const backwardCompatibilityChecker: Checker<string> = match(
+ *   string(),
+ *   asType(number(), num => `${num}`),
+ *   asType(record({num: number()}), obj => `${obj.num}`),
+ * );
+ * ```
+ */
+export declare function match<T>(
+  ...checkers: ReadonlyArray<Checker<T>>
+): Checker<T>;
+
+/**
+ * wraps a given checker, making the valid value nullable
+ *
+ * By default, a value passed to nullable must match the checker spec exactly
+ * when it is not null, or it will fail.
+ *
+ * passing the `nullWithWarningWhenInvalid` enables gracefully handling invalid
+ * values that are less important -- if the provided checker is invalid,
+ * the new checker will return null.
+ *
+ * For example:
+ *
+ * ```javascript
+ * import {nullable, record, string} from 'refine';
+ *
+ * const Options = record({
+ *   // this must be a non-null string,
+ *   // or Options is not valid
+ *   filename: string(),
+ *
+ *   // if this field is not a string,
+ *   // it will be null and Options will pass the checker
+ *   description: nullable(string(), {
+ *     nullWithWarningWhenInvalid: true,
+ *   })
+ * })
+ *
+ * const result = Options({filename: 'test', description: 1});
+ *
+ * invariant(result.type === 'success');
+ * invariant(result.value.description === null);
+ * invariant(result.warnings.length === 1); // there will be a warning
+ * ```
+ */
+export declare function nullable<T>(
+  checker: Checker<T>,
+  options?: Readonly<{
+    // if this is true, the checker will not fail
+    // validation if an invalid value is provided, instead
+    // returning null and including a warning as to the invalid type.
+    nullWithWarningWhenInvalid?: boolean;
+  }>,
+): Checker<T | null | void>;
+
+/**
+ * wraps a given checker, making the valid value voidable
+ *
+ * By default, a value passed to voidable must match the checker spec exactly
+ * when it is not undefined, or it will fail.
+ *
+ * passing the `undefinedWithWarningWhenInvalid` enables gracefully handling invalid
+ * values that are less important -- if the provided checker is invalid,
+ * the new checker will return undefined.
+ *
+ * For example:
+ *
+ * ```javascript
+ * import {voidable, record, string} from 'refine';
+ *
+ * const Options = record({
+ *   // this must be a string, or Options is not valid
+ *   filename: string(),
+ *
+ *   // this must be a string or undefined,
+ *   // or Options is not valid
+ *   displayName: voidable(string()),
+ *
+ *   // if this field is not a string,
+ *   // it will be undefined and Options will pass the checker
+ *   description: voidable(string(), {
+ *     undefinedWithWarningWhenInvalid: true,
+ *   })
+ * })
+ *
+ * const result = Options({filename: 'test', description: 1});
+ *
+ * invariant(result.type === 'success');
+ * invariant(result.value.description === undefined);
+ * invariant(result.warnings.length === 1); // there will be a warning
+ * ```
+ */
+export declare function voidable<T>(
+  checker: Checker<T>,
+  options?: Readonly<{
+    // if this is true, the checker will not fail
+    // validation if an invalid value is provided, instead
+    // returning undefined and including a warning as to the invalid type.
+    undefinedWithWarningWhenInvalid?: boolean;
+  }>,
+): Checker<T | void>;
+
+/**
+ * a checker that provides a withDefault value if the provided value is nullable.
+ *
+ * For example:
+ * ```jsx
+ * const objPropertyWithDefault = record({
+ *   foo: withDefault(number(), 123),
+ * });
+ * ```
+ * Both `{}` and `{num: 123}` will refine to `{num: 123}`
+ */
+export declare function withDefault<T>(
+  checker: Checker<T>,
+  fallback: T,
+): Checker<T>;
+
+/**
+ * wraps a checker with a logical constraint.
+ *
+ * Predicate function can return either a boolean result or
+ * a tuple with a result and message
+ *
+ * For example:
+ *
+ * ```javascript
+ * import {number, constraint} from 'refine';
+ *
+ * const evenNumber = constraint(
+ *   number(),
+ *   n => n % 2 === 0
+ * );
+ *
+ * const passes = evenNumber(2);
+ * // passes.type === 'success';
+ *
+ * const fails = evenNumber(1);
+ * // fails.type === 'failure';
+ * ```
+ */
+export declare function constraint<T>(
+  checker: Checker<T>,
+  predicate: (value: T) => boolean | [boolean, string],
+): Checker<T>;
+
+/**
+ * wrapper to allow for passing a lazy checker value. This enables
+ * recursive types by allowing for passing in the returned value of
+ * another checker. For example:
+ *
+ * ```javascript
+ * const user = record({
+ *   id: number(),
+ *   name: string(),
+ *   friends: array(lazy(() => user))
+ * });
+ * ```
+ *
+ * Example of array with arbitrary nesting depth:
+ * ```jsx
+ * const entry = or(number(), array(lazy(() => entry)));
+ * const nestedArray = array(entry);
+ * ```
+ */
+export declare function lazy<T>(getChecker: () => Checker<T>): Checker<T>;
+
+/**
+ * helper to create a custom checker from a provided function.
+ * If the function returns a non-nullable value, the checker succeeds.
+ *
+ * ```jsx
+ * const myClassChecker = custom(x => x instanceof MyClass ? x : null);
+ * ```
+ *
+ * Nullable custom types can be created by composing with `nullable()` or
+ * `voidable()` checkers:
+ *
+ * ```jsx
+ * const maybeMyClassChecker =
+ *   nullable(custom(x => x instanceof MyClass ? x : null));
+ * ```
+ */
+export declare function custom<T>(
+  checkValue: (value: unknown) => null | T,
+  failureMessage?: string,
+): Checker<T>;
