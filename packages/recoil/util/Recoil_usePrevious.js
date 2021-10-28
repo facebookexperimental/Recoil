cf2b2e57e734f884bfd4133cd37fe852e0ddd711
Douglas Armstrong <dougarmstrong@fb.com>
2021-10-28 15:19:03 -0700

    Split useRetain() to a separate file
    Summary: Simplify large `Recoil_Hooks.js` by splitting `useRetain()` to a separate file.
    
    Differential Revision: https://www.internalfb.com/diff/D31900811?entry_point=27
    
    fbshipit-source-id: a14c6f69f57c059aa21ef49ba98c1b2eff4d82ce

---
+++ packages/recoil/util/Recoil_usePrevious.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails oncall+recoil
+ * @flow strict-local
+ * @format
+ */
+'use strict';
+
+const {useEffect, useRef} = require('react');
+
+function usePrevious<T>(value: T): T | void {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+module.exports = usePrevious;
