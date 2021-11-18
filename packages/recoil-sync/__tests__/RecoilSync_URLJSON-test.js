cff92f53b80bcd107c37886aeaed4a240599bd80
Douglas Armstrong <dougarmstrong@fb.com>
2021-11-18 14:55:56 -0800

    Test encoding/decoding Dates in JSON URL (#1353)
    Summary:
    Pull Request resolved: https://github.com/facebookexperimental/Recoil/pull/1353
    
    Test encoding/decoding Dates in JSON URL
    
    Differential Revision: https://www.internalfb.com/diff/D31953806?entry_point=27
    
    fbshipit-source-id: 3468f8ee174ddd528e8060a3f8e159bdfad9c6c0

--- packages/recoil-sync/__tests__/RecoilSync_URLJSON-test.js
+++ packages/recoil-sync/__tests__/RecoilSync_URLJSON-test.js
@@ -17,7 +17,15 @@ const {
 const {syncEffect} = require('../RecoilSync');
 const {RecoilURLSyncJSON} = require('../RecoilSync_URLJSON');
 const React = require('react');
-const {array, boolean, number, object, string, tuple} = require('refine');
+const {
+  array,
+  boolean,
+  jsonDate,
+  number,
+  object,
+  string,
+  tuple,
+} = require('refine');
 
 const atomBoolean = atom({
   key: 'boolean',
@@ -48,6 +56,11 @@ const atomObject = atom({
     syncEffect({refine: object({foo: array(number())}), syncDefault: true}),
   ],
 });
+const atomDate = atom({
+  key: 'date',
+  default: new Date('October 26, 1985'),
+  effects_UNSTABLE: [syncEffect({refine: jsonDate(), syncDefault: true})],
+});
 
 async function testJSON(loc, contents, beforeURL, afterURL) {
   history.replaceState(null, '', beforeURL);
@@ -60,6 +73,7 @@ async function testJSON(loc, contents, beforeURL, afterURL) {
       <ReadsAtom atom={atomString} />
       <ReadsAtom atom={atomArray} />
       <ReadsAtom atom={atomObject} />
+      <ReadsAtom atom={atomDate} />
     </>,
   );
   expect(container.textContent).toBe(contents);
@@ -71,30 +85,30 @@ describe('URL JSON Encode', () => {
   test('Anchor', async () =>
     testJSON(
       {part: 'hash'},
-      'true123"STRING"[1,"a"]{"foo":[1,2]}',
+      'true123"STRING"[1,"a"]{"foo":[1,2]}"1985-10-26T07:00:00.000Z"',
       '/path/page.html?foo=bar',
-      '/path/page.html?foo=bar#%7B%22boolean%22%3Atrue%2C%22number%22%3A123%2C%22string%22%3A%22STRING%22%2C%22array%22%3A%5B1%2C%22a%22%5D%2C%22object%22%3A%7B%22foo%22%3A%5B1%2C2%5D%7D%7D',
+      '/path/page.html?foo=bar#%7B%22boolean%22%3Atrue%2C%22number%22%3A123%2C%22string%22%3A%22STRING%22%2C%22array%22%3A%5B1%2C%22a%22%5D%2C%22object%22%3A%7B%22foo%22%3A%5B1%2C2%5D%7D%2C%22date%22%3A%221985-10-26T07%3A00%3A00.000Z%22%7D',
     ));
   test('Search', async () =>
     testJSON(
       {part: 'search'},
-      'true123"STRING"[1,"a"]{"foo":[1,2]}',
+      'true123"STRING"[1,"a"]{"foo":[1,2]}"1985-10-26T07:00:00.000Z"',
       '/path/page.html#anchor',
-      '/path/page.html?%7B%22boolean%22%3Atrue%2C%22number%22%3A123%2C%22string%22%3A%22STRING%22%2C%22array%22%3A%5B1%2C%22a%22%5D%2C%22object%22%3A%7B%22foo%22%3A%5B1%2C2%5D%7D%7D#anchor',
+      '/path/page.html?%7B%22boolean%22%3Atrue%2C%22number%22%3A123%2C%22string%22%3A%22STRING%22%2C%22array%22%3A%5B1%2C%22a%22%5D%2C%22object%22%3A%7B%22foo%22%3A%5B1%2C2%5D%7D%2C%22date%22%3A%221985-10-26T07%3A00%3A00.000Z%22%7D#anchor',
     ));
   test('Query Params', async () =>
     testJSON(
       {part: 'queryParams'},
-      'true123"STRING"[1,"a"]{"foo":[1,2]}',
+      'true123"STRING"[1,"a"]{"foo":[1,2]}"1985-10-26T07:00:00.000Z"',
       '/path/page.html#anchor',
-      '/path/page.html?boolean=true&number=123&string=%22STRING%22&array=%5B1%2C%22a%22%5D&object=%7B%22foo%22%3A%5B1%2C2%5D%7D#anchor',
+      '/path/page.html?boolean=true&number=123&string=%22STRING%22&array=%5B1%2C%22a%22%5D&object=%7B%22foo%22%3A%5B1%2C2%5D%7D&date=%221985-10-26T07%3A00%3A00.000Z%22#anchor',
     ));
   test('Query Param', async () =>
     testJSON(
       {part: 'queryParams', param: 'param'},
-      'true123"STRING"[1,"a"]{"foo":[1,2]}',
+      'true123"STRING"[1,"a"]{"foo":[1,2]}"1985-10-26T07:00:00.000Z"',
       '/path/page.html?foo=bar#anchor',
-      '/path/page.html?foo=bar&param=%7B%22boolean%22%3Atrue%2C%22number%22%3A123%2C%22string%22%3A%22STRING%22%2C%22array%22%3A%5B1%2C%22a%22%5D%2C%22object%22%3A%7B%22foo%22%3A%5B1%2C2%5D%7D%7D#anchor',
+      '/path/page.html?foo=bar&param=%7B%22boolean%22%3Atrue%2C%22number%22%3A123%2C%22string%22%3A%22STRING%22%2C%22array%22%3A%5B1%2C%22a%22%5D%2C%22object%22%3A%7B%22foo%22%3A%5B1%2C2%5D%7D%2C%22date%22%3A%221985-10-26T07%3A00%3A00.000Z%22%7D#anchor',
     ));
 });
 
@@ -102,29 +116,29 @@ describe('URL JSON Parse', () => {
   test('Anchor', async () =>
     testJSON(
       {part: 'hash'},
-      'false456"SET"[2,"b"]{"foo":[]}',
-      '/#{"boolean":false,"number":456,"string":"SET","array":[2,"b"],"object":{"foo":[]}}',
-      '/#%7B%22boolean%22%3Afalse%2C%22number%22%3A456%2C%22string%22%3A%22SET%22%2C%22array%22%3A%5B2%2C%22b%22%5D%2C%22object%22%3A%7B%22foo%22%3A%5B%5D%7D%7D',
+      'false456"SET"[2,"b"]{"foo":[]}"1955-11-05T07:00:00.000Z"',
+      '/#{"boolean":false,"number":456,"string":"SET","array":[2,"b"],"object":{"foo":[]},"date":"1955-11-05T07:00:00.000Z"}',
+      '/#%7B%22boolean%22%3Afalse%2C%22number%22%3A456%2C%22string%22%3A%22SET%22%2C%22array%22%3A%5B2%2C%22b%22%5D%2C%22object%22%3A%7B%22foo%22%3A%5B%5D%7D%2C%22date%22%3A%221955-11-05T07%3A00%3A00.000Z%22%7D',
     ));
   test('Search', async () =>
     testJSON(
       {part: 'search'},
-      'false456"SET"[2,"b"]{"foo":[]}',
-      '/?{"boolean":false,"number":456,"string":"SET","array":[2,"b"],"object":{"foo":[]}}',
-      '/?%7B%22boolean%22%3Afalse%2C%22number%22%3A456%2C%22string%22%3A%22SET%22%2C%22array%22%3A%5B2%2C%22b%22%5D%2C%22object%22%3A%7B%22foo%22%3A%5B%5D%7D%7D',
+      'false456"SET"[2,"b"]{"foo":[]}"1955-11-05T07:00:00.000Z"',
+      '/?{"boolean":false,"number":456,"string":"SET","array":[2,"b"],"object":{"foo":[]},"date":"1955-11-05T07:00:00.000Z"}',
+      '/?%7B%22boolean%22%3Afalse%2C%22number%22%3A456%2C%22string%22%3A%22SET%22%2C%22array%22%3A%5B2%2C%22b%22%5D%2C%22object%22%3A%7B%22foo%22%3A%5B%5D%7D%2C%22date%22%3A%221955-11-05T07%3A00%3A00.000Z%22%7D',
     ));
   test('Query Params', async () =>
     testJSON(
       {part: 'queryParams'},
-      'false456"SET"[2,"b"]{"foo":[]}',
-      '/?boolean=false&number=456&string="SET"&array=[2,"b"]&object={"foo":[]}',
-      '/?boolean=false&number=456&string=%22SET%22&array=%5B2%2C%22b%22%5D&object=%7B%22foo%22%3A%5B%5D%7D',
+      'false456"SET"[2,"b"]{"foo":[]}"1955-11-05T07:00:00.000Z"',
+      '/?boolean=false&number=456&string="SET"&array=[2,"b"]&object={"foo":[]}&date="1955-11-05T07:00:00.000Z"',
+      '/?boolean=false&number=456&string=%22SET%22&array=%5B2%2C%22b%22%5D&object=%7B%22foo%22%3A%5B%5D%7D&date=%221955-11-05T07%3A00%3A00.000Z%22',
     ));
   test('Query Param', async () =>
     testJSON(
       {part: 'queryParams', param: 'param'},
-      'false456"SET"[2,"b"]{"foo":[]}',
-      '/?param={"boolean":false,"number":456,"string":"SET","array":[2,"b"],"object":{"foo":[]}}',
-      '/?param=%7B%22boolean%22%3Afalse%2C%22number%22%3A456%2C%22string%22%3A%22SET%22%2C%22array%22%3A%5B2%2C%22b%22%5D%2C%22object%22%3A%7B%22foo%22%3A%5B%5D%7D%7D',
+      'false456"SET"[2,"b"]{"foo":[]}"1955-11-05T07:00:00.000Z"',
+      '/?param={"boolean":false,"number":456,"string":"SET","array":[2,"b"],"object":{"foo":[]},"date":"1955-11-05T07:00:00.000Z"}',
+      '/?param=%7B%22boolean%22%3Afalse%2C%22number%22%3A456%2C%22string%22%3A%22SET%22%2C%22array%22%3A%5B2%2C%22b%22%5D%2C%22object%22%3A%7B%22foo%22%3A%5B%5D%7D%2C%22date%22%3A%221955-11-05T07%3A00%3A00.000Z%22%7D',
     ));
 });
