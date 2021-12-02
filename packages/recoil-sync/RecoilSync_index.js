5627ee265ffee309889f9ba576aa28ff350f73f3
Douglas Armstrong <dougarmstrong@fb.com>
2021-12-02 14:14:10 -0800

    Export options types (#1435)
    Summary:
    Pull Request resolved: https://github.com/facebookexperimental/Recoil/pull/1435
    
    Export the Flow types for options for `recoil-sync` package.
    
    Reviewed By: mondaychen
    
    Differential Revision: D32484870
    
    fbshipit-source-id: 2d92c97311821e31de592ba74b4013396860091b

--- packages/recoil-sync/RecoilSync_index.js
+++ packages/recoil-sync/RecoilSync_index.js
@@ -10,8 +10,25 @@
  */
 'use strict';
 
+import type {
+  ItemKey,
+  ListenToItems,
+  ReadAtom,
+  ReadAtomInterface,
+  ReadItem,
+  RecoilSyncOptions,
+  StoreKey,
+  SyncEffectOptions,
+  WriteAtom,
+  WriteAtomInterface,
+  WriteItems,
+} from './RecoilSync';
 import type {RecoilURLSyncOptions} from './RecoilSync_URL';
 import type {RecoilURLSyncJSONOptions} from './RecoilSync_URLJSON';
+import type {
+  RecoilURLSyncTransitOptions,
+  TransitHandler,
+} from './RecoilSync_URLTransit';
 
 const {RecoilSync, syncEffect, useRecoilSync} = require('./RecoilSync');
 const {
@@ -23,9 +40,32 @@ const {
   RecoilURLSyncJSON,
   useRecoilURLSyncJSON,
 } = require('./RecoilSync_URLJSON');
+const {
+  RecoilURLSyncTransit,
+  useRecoilURLSyncTransit,
+} = require('./RecoilSync_URLTransit');
 
-export type {RecoilURLSyncOptions};
-export type {RecoilURLSyncJSONOptions};
+export type {
+  // Keys
+  ItemKey,
+  StoreKey,
+  // Core useRecoilSync() options
+  RecoilSyncOptions,
+  ReadItem,
+  WriteItems,
+  ListenToItems,
+  // Core syncEffect() options
+  SyncEffectOptions,
+  ReadAtomInterface,
+  ReadAtom,
+  WriteAtomInterface,
+  WriteAtom,
+  // URL Synchronization
+  RecoilURLSyncOptions,
+  RecoilURLSyncJSONOptions,
+  RecoilURLSyncTransitOptions,
+  TransitHandler,
+};
 
 module.exports = {
   // Core Recoil Sync
@@ -36,7 +76,9 @@ module.exports = {
   // Recoil Sync URL
   useRecoilURLSync,
   useRecoilURLSyncJSON,
+  useRecoilURLSyncTransit,
   RecoilURLSync,
   RecoilURLSyncJSON,
+  RecoilURLSyncTransit,
   urlSyncEffect,
 };
