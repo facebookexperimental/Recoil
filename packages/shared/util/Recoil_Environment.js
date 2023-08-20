60c98550b61f5ba4b5b2c47b2c134cd88f40c8ea
Ted Lorts <tplorts@gmail.com>
2023-08-20 08:29:03 -0700

    only set isSSR true if !isReactNative

--- packages/shared/util/Recoil_Environment.js
+++ packages/shared/util/Recoil_Environment.js
@@ -11,19 +11,21 @@
 
 'use strict';
 
+const isReactNative: boolean =
+  typeof navigator !== 'undefined' && navigator.product === 'ReactNative'; // eslint-disable-line fb-www/typeof-undefined
+
 /* eslint-disable fb-www/typeof-undefined */
 const isSSR: boolean =
-  // $FlowFixMe(site=recoil) Window does not have a FlowType definition https://github.com/facebook/flow/issues/6709
-  typeof Window === 'undefined' || typeof window === 'undefined';
+  (!isReactNative &&
+    // $FlowFixMe(site=recoil) Window does not have a FlowType definition https://github.com/facebook/flow/issues/6709
+    typeof Window === 'undefined') ||
+  typeof window === 'undefined';
 /* eslint-enable fb-www/typeof-undefined */
 
 const isWindow = (value: mixed): boolean =>
   !isSSR &&
   // $FlowFixMe(site=recoil) Window does not have a FlowType definition https://github.com/facebook/flow/issues/6709
   (value === window || value instanceof Window);
-
-const isReactNative: boolean =
-  typeof navigator !== 'undefined' && navigator.product === 'ReactNative'; // eslint-disable-line fb-www/typeof-undefined
 
 module.exports = {
   isSSR,
